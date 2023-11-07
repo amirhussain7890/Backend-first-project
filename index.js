bd3788dba0f06b78348bb3f5bfb79fea43322310
Amir Hussain <amir1995hu@gmail.com>
2023-11-07 18:57:42 +0530

    add new project

---
+++ index.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config()
+
+const express=require("express");
+const app=express();
+const port=4000;
+
+app.get("/",(req,res)=>{
+    res.send("hey there!")
+})
+
+app.get("/mytwitter",(req,res)=>{
+    res.send("twitter tweets")
+})
+
+app.get("/login",(req,res)=>{
+    res.send('<h1> Please login</h1>');
+})
+
+app.get(("/youtube",(req,res)=>{
+    res.send("<h2>Pls subscribe</h2>")
+}))
+app.listen(process.env.PORT,()=>{
+    console.log(` Example app listening on port ${port}`)
+})
+
